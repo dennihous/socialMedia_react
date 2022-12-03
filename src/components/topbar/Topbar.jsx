2e70d2ef746e48a8749d6b3e8a6074b6f9260507
dennis <denhouston1@icloud.com>
2022-12-03 12:03:47 +0000

    Top bar start

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -4,7 +4,8 @@ export default function Topbar() {
   return (
     <div className="topbarContainer">
         <div className="topbarLeft"></div>
-
+        <div className="topbarCenter"></div>
+        <div className="topbarRight"></div>
     </div>
   )
 }
