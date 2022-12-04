3d5ddaba29e8ee358246dd68f9e73810026c56b5
dennis <denhouston1@icloud.com>
2022-12-04 19:13:04 +0000

    Added icons for person, chat and notifications

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,8 @@
 import "./topbar.css"
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 export default function Topbar() {
   return (
@@ -6,8 +10,31 @@ export default function Topbar() {
         <div className="topbarLeft">
           <span className="logo">Dennis Social</span>
         </div>
-        <div className="topbarCenter"></div>
-        <div className="topbarRight"></div>
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <SearchIcon />
+            <input placeholder="Search" className="searchInput" />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLink">Homepage</span>
+            <span className="topbarLink">Timeline</span>
+          </div>
+          <div className="topbarIcon">
+            <div className="topbarIconItem">
+              <PersonIcon />
+              <span className="topbarIconBardge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <ChatBubbleIcon />
+              <span className="topbarIconBardge">1</span>
+            </div><div className="topbarIconItem">
+              <NotificationsIcon />
+              <span className="topbarIconBardge">1</span>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
