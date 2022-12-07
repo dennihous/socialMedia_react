746ec092c361a13519a30858039b4a8503b7ca70
dennis <denhouston1@icloud.com>
2022-12-07 14:00:00 +0100

    Updating sidebar css

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,9 @@ import './sidebar.css'
 export default function Sidebar() {
   return  (
     <div className='sidebar'>
+      <div className="sidevarWrapper">
+        
+      </div>
       sidebar
     </div>
   )
