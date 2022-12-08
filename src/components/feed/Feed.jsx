6a121c5b101ef3d4206b16b3dd648a87af253fd8
dennis <denhouston1@icloud.com>
2022-12-08 14:00:00 +0100

    Added feed wrapper

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,7 +3,9 @@ import './feed.css'
 export default function Feed() {
   return  (
     <div className='feed'>
-      feed
+      <div className="feedWrapper">
+        
+      </div>
     </div>
   )
 }
