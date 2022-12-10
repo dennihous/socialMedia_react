b2e875e8fb6796b9be8faf816fa314a7ea5efd98
dennis <denhouston1@icloud.com>
2022-12-10 12:45:21 +0000

    Added sidebar css and jsx

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,17 @@
 import './sidebar.css'
+import RssFeedIcon from '@mui/icons-material/RssFeed';
 
 export default function Sidebar() {
   return  (
     <div className='sidebar'>
       <div className="sidebarWrapper">
-        hello
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <RssFeedIcon className='sidebarIcon'/>
+            <span className="sidebarListItemText">Feed</span>
+          </li>
+          
+        </ul>
       </div>
     </div>
   )
