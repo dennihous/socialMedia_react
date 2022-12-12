c990ff5128a91ee52eef708c6f809565cbcd0c2f
dennis <denhouston1@icloud.com>
2022-12-12 14:00:00 +0100

    Added rightbar birthday icon

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,17 @@
 import './rightbar.css'
+import CakeIcon from '@mui/icons-material/Cake';
 
 export default function Rightbar() {
   return  (
     <div className='rightbar'>
-      rightbar
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <CakeIcon className='birthdayImage' />
+          <span className="birthdayText">
+            <b>Dennis</b> and <b>3 other friends</b> have their birthday today
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
