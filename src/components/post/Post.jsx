2464bb9c305bd3032f04ee2111a2a3df0dcab431
dennis <denhouston1@icloud.com>
2022-12-13 23:16:23 +0000

    Added profile pic prop

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -15,7 +15,7 @@ export default function Post({post}) {
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImage" src="/assets/profilePic.jpg" alt="" />
+            <img className="postProfileImage" src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt="" />
             <span className="postUsername">{Users.filter(u => u.id === post.userId)[0].username}</span>
             <span className="postDate">{post.date}</span>
           </div>
