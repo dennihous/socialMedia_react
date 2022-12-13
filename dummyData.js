5d2a12a28314dfa0d5b3080a5d17cbd8f0a91d42
dennis <denhouston1@icloud.com>
2022-12-13 22:19:12 +0000

    Added user dummy data

---
+++ dummyData.js
@@ -0,0 +1,53 @@
+export const Users = [
+  {
+    id: 1,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis1"
+  },
+  {
+    id: 2,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis2"
+  },
+  {
+    id: 3,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis3"
+  },
+  {
+    id: 4,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis4"
+  },
+  {
+    id: 5,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis5"
+  },
+  {
+    id: 6,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis6"
+  },
+  {
+    id: 7,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis1"
+  },
+  {
+    id: 8,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis2"
+  },
+  {
+    id: 9,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis3"
+  },
+  {
+    id: 10,
+    profilePicture: "assets/profilePic.jpg",
+    username: "Dennis4"
+  },
+];
+
