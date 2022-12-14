a3ab83f56dbc8fc3fb85ca87bfab7bdd5bd7c5c0
dennis <denhouston1@icloud.com>
2022-12-14 15:09:21 +0000

    App.js now shows home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import Profile from "./pages/profile/Profile";
-// import Home from "./pages/home/Home";
+import Home from "./pages/home/Home";
 // import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+// import Register from "./pages/register/Register";
 
 function App() {
-  return <Register />
+  return <Home />
 }
 
 export default App;
