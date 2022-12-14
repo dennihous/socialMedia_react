e7601966446ddfb4aac08ede53d7dc638c4b913c
dennis <denhouston1@icloud.com>
2022-12-14 14:48:04 +0000

    Added login input

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -10,7 +10,11 @@ export default function Login() {
         </div>
         <div className="loginRight">
           <div className="loginBox">
-
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <button className="loginButon">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">Create new account</button>
           </div>
         </div>
       </div>
