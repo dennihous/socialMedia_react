28d66f031df604a8a26ec68c606c84bd395dbefa
dennis <denhouston1@icloud.com>
2022-12-14 13:13:27 +0000

    Added profile flex styling

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,9 +1,25 @@
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
 import "./profile.css"
 
 export default function Profile() {
   return (
+    <>
     <div>
-      hey
+      <Topbar />
+      <div className="profile">
+        <Sidebar />
+        <div className="profileRight">
+          <div className="profileRightTop">j</div>
+          <div className="profileRightBottom">
+            <Feed />
+            <Rightbar />
+          </div>
+        </div>
+      </div>
     </div>
+    </>
   )
 }
