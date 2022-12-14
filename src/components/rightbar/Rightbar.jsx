22024734507e9d1989220d55344c585fb57074c4
dennis <denhouston1@icloud.com>
2022-12-14 14:01:36 +0000

    Added profile page followers

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,11 +3,11 @@ import CakeIcon from '@mui/icons-material/Cake';
 import { Users } from '../../dummyData';
 import Online from '../online/Online';
 
-export default function Rightbar() {
-  return  (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+export default function Rightbar({profile}) {
+  const HomeRightbar = () => {
+    return (
+      <>
+         <div className="birthdayContainer">
           <CakeIcon className='birthdayImage' />
           <span className="birthdayText">
             <b>Dennis</b> and <b>3 other friends</b> have their birthday today.
@@ -20,6 +20,73 @@ export default function Rightbar() {
             <Online key={u.id} user={u} />
           ))}
         </ul>
+      </>
+    )
+  }
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User info title</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City: </span>
+            <span className="rightbarInfoValue">London</span> 
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From: </span>
+            <span className="rightbarInfoValue">Ireland</span> 
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Gender: </span>
+            <span className="rightbarInfoValue">Male</span> 
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+        <div className="rightbarFollowers">
+          <div className="rightbarFollower">
+            <img src="assets/profilePic.jpg" alt="" className="rightbarFollowerImage" />
+            <span className='rightbarFollowerName'>Dennis</span>
+          </div>
+        </div>
+      </>
+    )
+  }
+  return  (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        
+        <ProfileRightbar />
       </div>
     </div>
   )
