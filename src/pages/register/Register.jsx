c26e852c5e191e5a891530ead068289ff31f40e9
dennis <denhouston1@icloud.com>
2022-12-15 16:01:02 +0000

    Added password invisibility

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -12,8 +12,8 @@ export default function Register() {
           <div className="loginBox">
             <input placeholder="Username" className="loginInput" />
             <input placeholder="Email" className="loginInput" />
-            <input placeholder="Password" className="loginInput" />
-            <input placeholder="Confirm Password" className="loginInput" />
+            <input type="password" placeholder="Password" className="loginInput" />
+            <input type="password" placeholder="Confirm Password" className="loginInput" />
             <button className="loginButton">Sign Up</button>
             <button className="loginRegisterButton">Log into your account</button>
           </div>
