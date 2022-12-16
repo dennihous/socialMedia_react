577c32971ab286a007caa06264adfb05d6049cdf
dennis <denhouston1@icloud.com>
2022-12-16 15:24:50 +0000

    Added register route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Profile from "./pages/profile/Profile";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register";
+import Register from "./pages/register/Register";
 import {
   BrowserRouter as Router,
   Switch,
@@ -20,6 +20,9 @@ function App() {
         </Route>
         <Route path="/login">
           <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
         </Route>
       </Switch>
     </Router>
