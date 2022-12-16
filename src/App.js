41480dfd6e807bb364d5c3cd40687048065a8d91
dennis <denhouston1@icloud.com>
2022-12-16 15:22:40 +0000

    Added home page route

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@
 import Home from "./pages/home/Home";
 // import Login from "./pages/login/Login";
 // import Register from "./pages/register/Register";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  return <Home />
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
