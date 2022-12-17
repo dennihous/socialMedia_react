608c187d1f5e60ac48901b6598a6e39eb32bb457
dennis <denhouston1@icloud.com>
2022-12-17 14:00:00 +0100

    Paths added for each route

--- src/App.js
+++ src/App.js
@@ -3,29 +3,22 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+  BrowserRouter,
+  Routes,
+  Route,
 } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <Home/>
-        </Route>
-        <Route path="/profile">
-          <Profile/>
-        </Route>
-        <Route path="/login">
-          <Login/>
-        </Route>
-        <Route path="/register">
-          <Register/>
-        </Route>
-      </Switch>
-    </Router>
+    // <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes> 
+    </BrowserRouter>
   )
 }
 
