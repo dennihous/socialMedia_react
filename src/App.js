a29bfc23f96e3bcba362f1cdbcbb7c160549a3cb
dennis <denhouston1@icloud.com>
2022-12-18 14:00:00 +0100

    BrowserRouter changed to Router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import {
-  BrowserRouter,
+  BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
@@ -11,14 +11,14 @@ import {
 function App() {
   return (
     // <Home/>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/profile" element={<Profile/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes> 
-    </BrowserRouter>
+    </Router>
   )
 }
 
