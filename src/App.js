bb94b85ddc00f4ec558e2248fa8e00f889b1239e
dennis <denhouston1@icloud.com>
2022-12-19 22:52:48 +0000

    Refactored app.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,21 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
 
 function App() {
   return (
-    // <Home/>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/profile/:username" element={<Profile/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes> 
-    </Router>
+    </BrowserRouter>
   )
 }
 
