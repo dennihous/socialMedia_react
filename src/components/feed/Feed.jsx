3c3ddcb47d813ae2e642b8fa5c0f996ceda92e0a
dennis <denhouston1@icloud.com>
2022-12-24 16:37:46 +0000

    Added axios request

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,16 +1,24 @@
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
-import {Posts} from '../../dummyData.js'
+import { useEffect, useState } from "react";
+import axios from "axios"
+// import {Posts} from '../../dummyData.js'
 
 export default function Feed() {
+  const [post, usePost] = useState([])
+
+  useEffect(() => {
+    axios.get("/timeline/")
+  }, [])
+
   return  (
     <div className='feed'>
       <div className="feedWrapper">
         <Share/>
-        { Posts.map(p => (
+        {/* { Posts.map(p => (
           <Post key={p.id} post={p}/>
-        ))}
+        ))} */}
       </div>
     </div>
   )
