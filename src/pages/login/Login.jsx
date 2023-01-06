4113c6d0e0dd514b76d3f23e70628e7fbcfc6165
dennis <denhouston1@icloud.com>
2023-01-06 17:28:50 +0000

    Added form so that non-emails are caught

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -9,13 +9,13 @@ export default function Login() {
           <span className="loginDescription">Connect with DenniSocial</span>
         </div>
         <div className="loginRight">
-          <div className="loginBox">
+          <form className="loginBox">
             <input placeholder="Email" type="email" className="loginInput" />
             <input placeholder="Password" type="password" className="loginInput" />
             <button className="loginButton">Log In</button>
             <span className="loginForgot">Forgot Password?</span>
             <button className="loginRegisterButton">Create new account</button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
