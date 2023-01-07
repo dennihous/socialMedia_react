e5f2fb9be9b71b3302492fbe399d8788ecb33b65
dennis <denhouston1@icloud.com>
2023-01-07 15:37:41 +0000

    Added AuthContext.Provider to share state

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,4 +10,10 @@ export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = () => {
   const [state, dispatch] = useReducer(AuthContext, INITIAL_STATE);
+
+  return (
+    <AuthContext.Provider value={{user:state.user, isFetching:state.isFetching, error:state.error, dispatch}}>
+
+    </AuthContext.Provider>
+  )
 }
