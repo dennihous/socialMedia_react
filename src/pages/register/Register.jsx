04ebfc7fc48989f336a85d805db176ef91fd14f6
dennis <denhouston1@icloud.com>
2023-01-09 14:03:19 +0000

    Made input fields neater

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,6 +1,6 @@
 import "./register.css"
 import { useRef } from "react";
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 export default function Register() {
@@ -22,7 +22,7 @@ export default function Register() {
       };
       try {
         await axios.post("/auth/register", user);
-        navigate("/login");
+        navigate("/");
       } catch(err){
         console.log(err);
       }
@@ -38,10 +38,33 @@ export default function Register() {
         </div>
         <div className="loginRight">
           <form className="loginBox" onSubmit={handleClick}>
-            <input placeholder="Username" required ref={username} className="loginInput" />
-            <input placeholder="Email" required ref={email} className="loginInput" type="email" />
-            <input placeholder="Password" required ref={password} className="loginInput" type="password" minLength="6"/>
-            <input placeholder="Confirm Password" required ref={confirmPassword} className="loginInput" type="password" />
+            <input 
+              placeholder="Username" 
+              required ref={username} 
+              className="loginInput" 
+            />
+            <input 
+              placeholder="Email" 
+              required 
+              ref={email} 
+              className="loginInput" 
+              type="email" 
+            />
+            <input 
+              placeholder="Password" 
+              required 
+              ref={password} 
+              className="loginInput" 
+              type="password" 
+              minLength="6"
+            />
+            <input 
+              placeholder="Confirm Password" 
+              required 
+              ref={confirmPassword} 
+              className="loginInput" 
+              type="password" 
+            />
             <button className="loginButton" type="submit">Sign Up</button>
             <button className="loginRegisterButton">Log into your account</button>
           </form>
