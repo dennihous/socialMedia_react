0f1fbf7b43a71cb6ea26879b196f7fb2a4c81cf5
dennis <denhouston1@icloud.com>
2023-01-10 19:54:47 +0000

    Added PF and useContext

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -3,13 +3,18 @@ import PermMediaIcon from '@mui/icons-material/PermMedia';
 import StyleIcon from '@mui/icons-material/Style';
 import RoomIcon from '@mui/icons-material/Room';
 import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Share() {
+  const {user} = useContext(AuthContext)
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
   return (
     <div className="share">
         <div className="shareWrapper">
           <div className="shareTop">
-            <img className="shareProfileImg" src="/assets/profilePic.jpg" alt="" />
+            <img className="shareProfileImg" src={user.profilePicture ? PF+user.profilePicture : PF+"profile-empty.webp"} alt="" />
             <input placeholder="What's on your mind?" className="shareInput" /> 
           </div>
           <hr className="shareHr" />
