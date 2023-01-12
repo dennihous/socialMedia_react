61f19b1e4c756bf6bd2e4bd80609a1869fb003c1
dennis <denhouston1@icloud.com>
2023-01-12 15:33:21 +0000

    Small changes

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -37,9 +37,11 @@ export default function Rightbar({user}) {
   const handleClick = async () => {
     try{
       if(followed){
-        await axios.put("/users/"+user._id+"/follow", {userId: currentUser._id})
+        await axios.put("/users/"+user._id+"/unfollow", {userId: currentUser._id});
+        dispatch({type: "UNFOLLOW", payload: user._id});
       } else {
-        await axios.put("/users/"+user._id+"/unfollow", {userId: currentUser._id})  
+        await axios.put("/users/"+user._id+"/follow", {userId: currentUser._id});
+        dispatch({type: "FOLLOW", payload: user._id});
       }
     } catch(err){
       console.log(err)
